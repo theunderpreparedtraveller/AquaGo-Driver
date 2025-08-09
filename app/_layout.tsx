f12341871faf314d2b36d4b5c3fbd4891ccd51f3
Your Name <you@example.com>
2025-08-09 13:09:00 +0530

    Added sentry

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,21 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useFonts, Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold } from '@expo-google-fonts/inter';
 import * as SplashScreen from 'expo-splash-screen';
+import * as Sentry from "@sentry/react-native";
 
+Sentry.init({
+  dsn: 'https://a336ec2be8c48c6df44db130255345a7@o4509456068902912.ingest.de.sentry.io/4509456071262288',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+export default Sentry.wrap(function RootLayout() {
   const [fontsLoaded] = useFonts({
     'Inter-Regular': Inter_400Regular,
     'Inter-Medium': Inter_500Medium,
@@ -32,4 +43,4 @@ export default function RootLayout() {
       <StatusBar style="dark" />
     </>
   );
-}
+});
